829446ad726ceb76dd60031cf6157e7b47774c93
Biliqis Onikoyi <biliqisonikoyi@gmail.com>
2026-03-04 22:58:48 +0100

    initialise dependencies

---
+++ src/test/java/com/biliqis/hafsahs_place/HafsahsPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biliqis.hafsahs_place;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HafsahsPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
