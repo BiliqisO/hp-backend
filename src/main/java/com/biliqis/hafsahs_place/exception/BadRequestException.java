8bac4165f5e9aaba90b5eddbae7de7d72f79ce12
Biliqis Onikoyi <biliqisonikoyi@gmail.com>
2026-03-05 13:32:43 +0100

    exception module

---
+++ src/main/java/com/biliqis/hafsahs_place/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.biliqis.hafsahs_place.exception;
+
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
